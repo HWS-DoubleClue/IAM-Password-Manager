d5e0ea3143ffcefde3e755b95c935c85fb8d9338
Mark.Herrmann <mark.herrmann@hws-gruppe.de>
2023-03-29 10:52:14 +0200

    security: improve password hash to bcrypt: adds BcryptUtils

---
+++ LibKaraUtils/src/main/java/com/doubleclue/utils/BcryptUtils.java
@@ -0,0 +1,45 @@
+package com.doubleclue.utils;
+
+import java.time.Duration;
+import java.time.Instant;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public final class BcryptUtils {
+	
+	private static final int DEFAULT_STRENGTH = 8;
+	public static final int OPTIMAL_STRENGTH = calculateOptimalStrength();
+	
+	private BcryptUtils() {
+	}
+	
+	public static String hash(String rawPassword) {
+		return new BCryptPasswordEncoder(OPTIMAL_STRENGTH).encode(rawPassword);
+	}
+	
+	public static boolean isValid(String rawPassword, String hash) {
+		return new BCryptPasswordEncoder().matches(rawPassword, hash);
+	}
+	
+	public static boolean needsNewHash(String oldHash) {
+		if (oldHash == null || oldHash.isEmpty()) {
+			return true;
+		}
+		return new BCryptPasswordEncoder(OPTIMAL_STRENGTH).upgradeEncoding(oldHash);
+	}
+	
+	private static int calculateOptimalStrength() {
+		int strength = DEFAULT_STRENGTH;
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(strength);
+		Instant start = Instant.now();
+		encoder.encode("durance-check");
+		Instant finish = Instant.now();
+		long durance = Duration.between(start, finish).toMillis();
+		while (durance < 500) {
+			System.out.println("running...");
+			strength++;
+			durance *= 2;
+		}
+		return strength;
+	}
+}
