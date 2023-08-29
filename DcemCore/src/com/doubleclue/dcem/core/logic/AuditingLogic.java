bf7a367c1db3846962b3460e7a1b984eb4f3e6b2
Maximilian Schmidt <maximilian.schmidt@hws-gruppe.de>
2023-08-29 13:10:11 +0200

    improved auditing and compare objects
    fixed cloudsafe upload, user is not superadmin

--- DcemCore/src/com/doubleclue/dcem/core/logic/AuditingLogic.java
+++ DcemCore/src/com/doubleclue/dcem/core/logic/AuditingLogic.java
@@ -9,7 +9,9 @@ import javax.persistence.Query;
 import com.doubleclue.dcem.core.entities.Auditing;
 import com.doubleclue.dcem.core.entities.DcemAction;
 import com.doubleclue.dcem.core.entities.DcemUser;
+import com.doubleclue.dcem.core.entities.EntityInterface;
 import com.doubleclue.dcem.core.jpa.DcemTransactional;
+import com.doubleclue.dcem.core.utils.DcemUtils;
 
 @ApplicationScoped
 @Named("auditingLogic")
@@ -30,6 +32,32 @@ public class AuditingLogic {
 			dcemAction = actionLogic.getDcemAction(dcemAction);
 		}
 		Auditing auditing = new Auditing(dcemAction, information, operatorSessionBean.getDcemUser());
+		em.persist(auditing);
+	}
+
+	@DcemTransactional
+	public void addAudit(DcemAction dcemAction, EntityInterface newEntity) {
+		if (dcemAction.getId() == null) {
+			dcemAction = actionLogic.getDcemAction(dcemAction);
+		}
+		String changeInfo;
+		if (newEntity.getId() == null) {
+			try {
+				EntityInterface oldEntity = newEntity.getClass().newInstance();
+				changeInfo = DcemUtils.compareObjects(oldEntity, newEntity, true);
+			} catch (Exception exp) {
+				changeInfo = newEntity.toString();
+			}
+		} else {
+			try {
+				EntityInterface oldEntity = em.find(newEntity.getClass(), newEntity.getId());
+				changeInfo = DcemUtils.compareObjects(oldEntity, newEntity);
+			} catch (Exception exp) {
+				// logger.warn("Couldn't compare operator", exp);
+				changeInfo = "ERROR: " + exp.getMessage();
+			}
+		}
+		Auditing auditing = new Auditing(dcemAction, changeInfo, operatorSessionBean.getDcemUser());
 		em.persist(auditing);
 	}
 	
