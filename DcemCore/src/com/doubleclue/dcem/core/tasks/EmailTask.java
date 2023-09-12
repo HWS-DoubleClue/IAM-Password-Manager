96d39c36a1100ea91982a545dbd3619674db6991
Galea, Emanuel <emanuel.galea@hws-gruppe.de>
2023-09-12 11:21:47 +0200

    Merge branch 'master' of https://github.com/HWS-DoubleClue/IAM-Password-Manager.git

--- DcemCore/src/com/doubleclue/dcem/core/tasks/EmailTask.java
+++ DcemCore/src/com/doubleclue/dcem/core/tasks/EmailTask.java
@@ -1,61 +1,39 @@
 package com.doubleclue.dcem.core.tasks;
 
-import java.util.Collection;
 import java.util.List;
-import java.util.concurrent.CountDownLatch;
+import java.util.Map;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import com.doubleclue.dcem.core.cluster.DcemCluster;
-import com.doubleclue.dcem.core.entities.TenantEntity;
+import com.doubleclue.dcem.core.entities.DcemUser;
 import com.doubleclue.dcem.core.gui.DcemApplicationBean;
-import com.doubleclue.dcem.core.jpa.TenantIdResolver;
-import com.doubleclue.dcem.core.logic.module.DcemModule;
 import com.doubleclue.dcem.core.weld.CdiUtils;
 
 public class EmailTask extends CoreTask {
 
 	private static final Logger logger = LogManager.getLogger(EmailTask.class);
 
-	CountDownLatch countDownLatch;
+	List<DcemUser> users;
+	Map<String, String> map;
+	String templateName;
 
-	public EmailTask() {
+	public EmailTask(List<DcemUser> users, Map<String, String> map, String templateName) {
 		super(EmailTask.class.getSimpleName(), null);
+		this.users = users;
+		this.map = map;
+		this.templateName = templateName;
 	}
+	
+	
 
 	@Override
 	public void runTask() {
-
-		if (DcemCluster.getDcemCluster().isClusterMaster() == false) {
-			return;
-		}
-		Thread.currentThread().setName(this.getClass().getSimpleName());
+		
 		logger.info("Nightly run started");
 		long start = System.currentTimeMillis();
 		DcemApplicationBean applicationBean = CdiUtils.getReference(DcemApplicationBean.class);
-		List<DcemModule> sortedModules = applicationBean.getSortedModules();
-		TaskExecutor taskExecutor = CdiUtils.getReference(TaskExecutor.class);
-		countDownLatch = new CountDownLatch(1);
-		taskExecutor.execute(new NightlyTaskTenant(TenantIdResolver.getMasterTenant(), sortedModules, countDownLatch));
-		try {
-			countDownLatch.await();
-		} catch (InterruptedException e) {
-			logger.warn(e);
-		}
-		Collection<TenantEntity> tenantEntities = applicationBean.getTenantMap().values();
-		for (TenantEntity tenantEntity : tenantEntities) {
-			if (tenantEntity.isMaster() == true) {
-				continue;
-			}
-			countDownLatch = new CountDownLatch(1);
-			taskExecutor.execute(new NightlyTaskTenant(tenantEntity, sortedModules, countDownLatch));
-			try {
-				countDownLatch.await();
-			} catch (InterruptedException e) {
-				logger.warn(e);
-			}
-		}
+			
 		logger.info("Nightly run ends: " + (System.currentTimeMillis() - start));
 	}
 
