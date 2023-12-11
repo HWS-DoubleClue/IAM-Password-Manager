5f99171d236a11f601ad3d2f23c39dab94edff4c
Galea, Emanuel <emanuel.galea@hws-gruppe.de>
2023-12-11 16:55:45 +0100

    add new savemultipleCloudSafe files

---
+++ AppSecureModule/src/com/doubleclue/dcem/as/logic/CloudSafeUploadFile.java
@@ -0,0 +1,19 @@
+package com.doubleclue.dcem.as.logic;
+
+import java.io.File;
+
+import com.doubleclue.dcem.as.entities.CloudSafeEntity;
+
+public class CloudSafeUploadFile {
+
+	public String fileName;
+	public File file;
+	CloudSafeEntity cloudSafeEntity;
+
+	public CloudSafeUploadFile(String fileName, File file, CloudSafeEntity cloudSafeEntity) {
+		this.fileName = fileName;
+		this.file = file;
+		this.cloudSafeEntity = cloudSafeEntity;
+	}
+
+}
