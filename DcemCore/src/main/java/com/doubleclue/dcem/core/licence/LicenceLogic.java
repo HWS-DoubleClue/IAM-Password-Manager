6f918bdc6926511f377be840b607ed2066b861e4
Maximilian Schmidt <maximilian.schmidt@hws-gruppe.de>
2024-05-28 15:33:44 +0200

    license check now extends to management

--- DcemCore/src/main/java/com/doubleclue/dcem/core/licence/LicenceLogic.java
+++ DcemCore/src/main/java/com/doubleclue/dcem/core/licence/LicenceLogic.java
@@ -93,9 +93,8 @@ public class LicenceLogic implements ReloadClassInterface {
 	@Inject
 	TaskExecutor taskExecutor;
 
-	private static final byte[] ENCRYPTION_KEY = { (byte) 0xAB, (byte) 0x32, (byte) 0x76, (byte) 0xFF, (byte) 0x3D,
-			(byte) 0xB4, (byte) 0x77, (byte) 0x29, (byte) 0x0E, (byte) 0x82, (byte) 0xD6, (byte) 0xB3, (byte) 0xBB,
-			(byte) 0x08, (byte) 0xC3, (byte) 0xD0 };
+	private static final byte[] ENCRYPTION_KEY = { (byte) 0xAB, (byte) 0x32, (byte) 0x76, (byte) 0xFF, (byte) 0x3D, (byte) 0xB4, (byte) 0x77, (byte) 0x29,
+			(byte) 0x0E, (byte) 0x82, (byte) 0xD6, (byte) 0xB3, (byte) 0xBB, (byte) 0x08, (byte) 0xC3, (byte) 0xD0 };
 
 	private static final String ENCRYPTION_ALGORITHM = "AES";
 	private static final String CIPHER_TYPE = "AES/CBC/PKCS5Padding";
@@ -106,42 +105,35 @@ public class LicenceLogic implements ReloadClassInterface {
 
 	// PRIVATE METHODS
 
-
 	public void checkForLicence(AuthApplication application, boolean allowChanceAfterExpiration) throws DcemException {
 		AdminTenantData adminTenantData = adminModule.getTenantData();
 		LicenceKeyContent licenceKeyContent = adminTenantData.getLicenceKeyContent();
-		
-		if (licenceKeyContent == null || userLogic.getTotalUserCount() >= licenceKeyContent.getMaxUsers()) {
-			if (allowChanceAfterExpiration) {
-				if (expiredLicenceUserShouldAuthenticate == true) {
-					expiredLicenceUserShouldAuthenticate = !expiredLicenceUserShouldAuthenticate;
-					throw new DcemException(DcemErrorCodes.LICENCE_MAX_USER,
-							"Maximum user licence reached MaxUsers:  " + userLogic.getTotalUserCount());
-				} else {
-					expiredLicenceUserShouldAuthenticate = !expiredLicenceUserShouldAuthenticate;
-				}
-			}
-		}
+
 		if (licenceKeyContent == null) {
 			return;
 		}
-		LocalDateTime expiryDate = licenceKeyContent.getLdtExpiresOn();
-		if (expiryDate.isBefore(LocalDateTime.now()) && (application == null || application != AuthApplication.DCEM)) {
-			boolean shouldAllow = false;
-			if (allowChanceAfterExpiration) {
-				LocalDateTime localDateTime = LocalDateTime.now().plusDays(LICENCE_WARNING_USER_THRESHOLD_PERCENTAGE);
-				if (expiryDate.isAfter(localDateTime)) {
-					shouldAllow = expiredLicenceUserShouldAuthenticate;
-					expiredLicenceUserShouldAuthenticate = !expiredLicenceUserShouldAuthenticate;
-				}
-			}
-			if (shouldAllow == false) {
+
+		boolean expiredLicence = licenceKeyContent.getLdtExpiresOn().isBefore(LocalDateTime.now());
+		if (expiredLicence) {
+			if (allowChanceAfterExpiration && expiredLicenceUserShouldAuthenticate) {
+				expiredLicenceUserShouldAuthenticate = !expiredLicenceUserShouldAuthenticate;
 				throw new DcemException(DcemErrorCodes.LICENCE_EXPIRED, "Cannot authenticate user - licence expired.");
 			}
 		}
+		
+		boolean exceedsUserCount = (userLogic.getTotalUserCount()*100000) >= licenceKeyContent.getMaxUsers();
+		if (exceedsUserCount) {
+			if (allowChanceAfterExpiration && expiredLicenceUserShouldAuthenticate) {
+					expiredLicenceUserShouldAuthenticate = !expiredLicenceUserShouldAuthenticate;
+					throw new DcemException(DcemErrorCodes.LICENCE_MAX_USER, "Maximum user licence reached MaxUsers:  " + userLogic.getTotalUserCount());
+				}
+			}
+
+		if (exceedsUserCount || expiredLicence) {
+			expiredLicenceUserShouldAuthenticate = !expiredLicenceUserShouldAuthenticate;
+		}
 	}
 
-	
 	// PUBLIC METHODS
 
 	public byte[] getEncryptedLicence(LicenceKeyContent licenceContent) throws DcemException {
@@ -159,8 +151,7 @@ public class LicenceLogic implements ReloadClassInterface {
 			return encryptedJson;
 		} catch (Exception e) {
 			logger.error(e);
-			throw new DcemException(DcemErrorCodes.INVALID_LICENCE_CONTENT,
-					"Could not encrypt Licence Content: " + licenceContent);
+			throw new DcemException(DcemErrorCodes.INVALID_LICENCE_CONTENT, "Could not encrypt Licence Content: " + licenceContent);
 		}
 	}
 
@@ -175,8 +166,7 @@ public class LicenceLogic implements ReloadClassInterface {
 			byte[] jsonBytes = cipher.doFinal(encryptedLicence);
 			String json = new String(jsonBytes, DcemConstants.CHARSET_UTF8);
 			LicenceKeyContent licenceKeyContent;
-			ObjectMapper objectMapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,
-					false);
+			ObjectMapper objectMapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 			licenceKeyContent = objectMapper.readValue(json, LicenceKeyContent.class);
 			return licenceKeyContent;
 		} catch (Exception e) {
@@ -184,7 +174,6 @@ public class LicenceLogic implements ReloadClassInterface {
 			throw new DcemException(DcemErrorCodes.INVALID_LICENCE_CONTENT, "Could not decrypt Licence Content. Please check your Cluster ID.");
 		}
 	}
-
 
 	public LicenceKeyContent getDecryptedLicence(String licenceKey) throws DcemException {
 		try {
@@ -196,15 +185,12 @@ public class LicenceLogic implements ReloadClassInterface {
 		}
 	}
 
-
 	public LicenceKeyContent loadLicenceKeyContent() throws DcemException {
 		try {
-			DcemConfiguration dcemConfiguration = configLogic.getDcemConfiguration(AdminModule.MODULE_ID,
-					DcemConstants.CONFIG_KEY_LICENCE);
+			DcemConfiguration dcemConfiguration = configLogic.getDcemConfiguration(AdminModule.MODULE_ID, DcemConstants.CONFIG_KEY_LICENCE);
 			AdminTenantData adminTenantData = adminModule.getAdminTenantData();
 			if (dcemConfiguration == null) {
-				LicenceKeyContent licenceKeyContent = createTrialLicence(DcemConstants.LICENCE_TRIAL_EXPIRY_DAYS,
-						"Trial Licence");
+				LicenceKeyContent licenceKeyContent = createTrialLicence(DcemConstants.LICENCE_TRIAL_EXPIRY_DAYS, "Trial Licence");
 				adminTenantData.setLicenceKeyContent(licenceKeyContent);
 				return licenceKeyContent;
 			}
@@ -235,11 +221,9 @@ public class LicenceLogic implements ReloadClassInterface {
 		}
 	}
 
-
 	public void resetExpiredLicenceUserShouldAuthenticate() {
 		expiredLicenceUserShouldAuthenticate = true;
 	}
-
 
 	public LicenceKeyContent createTrialLicence(int days, String customerName) throws DcemException {
 		Calendar c = Calendar.getInstance();
@@ -247,10 +231,9 @@ public class LicenceLogic implements ReloadClassInterface {
 		c.add(Calendar.DAY_OF_YEAR, days);
 		Date expiryDate = c.getTime();
 		String clusterId = DcemCluster.getDcemCluster().getClusterConfig().getName();
-		return new LicenceKeyContent(clusterId, customerName, expiryDate, TenantIdResolver.getCurrentTenantName(),
-				DcemConstants.LICENCE_KEY_VERSION, DcemConstants.LICENCE_PLUGINS_ALL);
+		return new LicenceKeyContent(clusterId, customerName, expiryDate, TenantIdResolver.getCurrentTenantName(), DcemConstants.LICENCE_KEY_VERSION,
+				DcemConstants.LICENCE_PLUGINS_ALL);
 	}
-
 
 	public String getEncryptedLicenceAsString(LicenceKeyContent licenceKeyContent) throws DcemException {
 		try {
@@ -261,7 +244,6 @@ public class LicenceLogic implements ReloadClassInterface {
 		}
 	}
 
-
 	public void addLicenceToDb(LicenceKeyContent licenceContent) throws DcemException {
 		String tenantId = licenceContent.tenantId;
 		if (((tenantId == null || tenantId.isEmpty()) && TenantIdResolver.isCurrentTenantMaster())
@@ -269,26 +251,22 @@ public class LicenceLogic implements ReloadClassInterface {
 			try {
 				byte[] encryptedLicence = getEncryptedLicence(licenceContent);
 				// check if there is already a licence saved for this module
-				DcemConfiguration semConfiguration = configLogic.getDcemConfiguration(AdminModule.MODULE_ID,
-						DcemConstants.CONFIG_KEY_LICENCE);
+				DcemConfiguration semConfiguration = configLogic.getDcemConfiguration(AdminModule.MODULE_ID, DcemConstants.CONFIG_KEY_LICENCE);
 				if (semConfiguration == null) {
-					semConfiguration = new DcemConfiguration(AdminModule.MODULE_ID, DcemConstants.CONFIG_KEY_LICENCE,
-							encryptedLicence);
+					semConfiguration = new DcemConfiguration(AdminModule.MODULE_ID, DcemConstants.CONFIG_KEY_LICENCE, encryptedLicence);
 				} else {
 					semConfiguration.setValue(encryptedLicence);
 				}
 				configLogic.setDcemConfiguration(semConfiguration);
 			} catch (Exception exp) {
 				logger.error("Couldn't create licence", exp);
-				throw new DcemException(DcemErrorCodes.INVALID_LICENCE_CONTENT,
-						"Could not set licence: " + licenceContent);
+				throw new DcemException(DcemErrorCodes.INVALID_LICENCE_CONTENT, "Could not set licence: " + licenceContent);
 			}
 		} else {
-			throw new DcemException(DcemErrorCodes.INVALID_LICENCE_CONTENT, "Wrong tenant while applying licence for: "
-					+ tenantId + " Current Tenant: " + TenantIdResolver.getCurrentTenantName());
+			throw new DcemException(DcemErrorCodes.INVALID_LICENCE_CONTENT,
+					"Wrong tenant while applying licence for: " + tenantId + " Current Tenant: " + TenantIdResolver.getCurrentTenantName());
 		}
 	}
-
 
 	public void setLicence(LicenceKeyContent licenceKeyContent, TenantEntity tenantEntity) throws Exception {
 		Future<Exception> future = taskExecutor.submit(new Callable<Exception>() {
@@ -347,25 +325,23 @@ public class LicenceLogic implements ReloadClassInterface {
 			int remainingUsers = maxUsers - userCount;
 			int threshold = (int) (((100f - LICENCE_WARNING_USER_THRESHOLD_PERCENTAGE) / 100f) * maxUsers);
 			if (remainingUsers < threshold) {
-				alerts.add(createAlertMessage(getLocalisedWarning("maxUsersAlmostReached", remainingUsers),
-						AlertSeverity.WARNING));
+				alerts.add(createAlertMessage(getLocalisedWarning("maxUsersAlmostReached", remainingUsers), AlertSeverity.WARNING));
 			}
 		}
-		
-		
+
 		DateFormat df = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT, operatorSessionBean.getLocale());
 		if (expiryDate.isBefore(now)) {
 			LocalDateTime localDateTimeExpire = expiryDate.plusDays(DcemConstants.LICENCE_ACTIVATION_EXPIRY_GRACE_PERIOD_DAYS);
-			
+
 			if (expiryDate.isBefore(localDateTimeExpire)) {
-				alerts.add(createAlertMessage(getLocalisedWarning("licenceExpiredGracePeriod", df.format(expiryDate)),	AlertSeverity.ERROR));
+				alerts.add(createAlertMessage(getLocalisedWarning("licenceExpiredGracePeriod", df.format(expiryDate)), AlertSeverity.ERROR));
 			} else {
-				alerts.add(createAlertMessage(getLocalisedWarning("licenceExpired", df.format(expiryDate)),	AlertSeverity.ERROR));
+				alerts.add(createAlertMessage(getLocalisedWarning("licenceExpired", df.format(expiryDate)), AlertSeverity.ERROR));
 			}
 		} else {
 			LocalDateTime localDateTimeWarinig = expiryDate.minusDays(DcemConstants.LICENCE_ACTIVATION_EXPIRY_GRACE_PERIOD_DAYS);
 			if (localDateTimeWarinig.isBefore(now)) {
-				alerts.add(createAlertMessage(getLocalisedWarning("licenceAlmostExpired", df.format(expiryDate)),	AlertSeverity.WARNING));
+				alerts.add(createAlertMessage(getLocalisedWarning("licenceAlmostExpired", df.format(expiryDate)), AlertSeverity.WARNING));
 			}
 		}
 		return alerts;
@@ -403,18 +379,16 @@ public class LicenceLogic implements ReloadClassInterface {
 					String email = admin.getEmail();
 					if (email != null && !email.isEmpty()) {
 						DbResourceBundle bundle = DbResourceBundle.getDbResourceBundle(admin.getLanguage().getLocale());
-						DcemTemplate bodyTemplate = templateLogic.getTemplateByNameLanguage(
-								DcemConstants.EMAIL_LICENCE_WARNING_BODY_TEMPLATE, admin.getLanguage());
+						DcemTemplate bodyTemplate = templateLogic.getTemplateByNameLanguage(DcemConstants.EMAIL_LICENCE_WARNING_BODY_TEMPLATE,
+								admin.getLanguage());
 						if (bodyTemplate == null) {
-							throw new DcemException(DcemErrorCodes.NO_TEMPLATE_FOUND,
-									"Missing template: " + DcemConstants.EMAIL_LICENCE_WARNING_BODY_TEMPLATE);
+							throw new DcemException(DcemErrorCodes.NO_TEMPLATE_FOUND, "Missing template: " + DcemConstants.EMAIL_LICENCE_WARNING_BODY_TEMPLATE);
 						}
 						Map<String, String> map = new HashMap<>();
 						map.put(DcemConstants.EMAIL_LICENCE_WARNING_KEY, moduleWarningsString);
 						map.put(DcemConstants.EMAIL_LICENCE_USER_KEY, admin.getDisplayNameOrLoginId());
 						String body = StringUtils.substituteTemplate(bodyTemplate.getContent(), map);
-						SendEmail.sendMessage(email, body,
-								bundle.getString(DcemConstants.EMAIL_LICENCE_WARNING_SUBJECT_BUNDLE_KEY));
+						SendEmail.sendMessage(email, body, bundle.getString(DcemConstants.EMAIL_LICENCE_WARNING_SUBJECT_BUNDLE_KEY));
 					}
 				}
 			}
@@ -432,21 +406,19 @@ public class LicenceLogic implements ReloadClassInterface {
 		alertMessage.setSeverity(severity);
 		alertMessage.setInfo(message);
 		alertMessage.setShowOnDashboard(true);
-		alertMessage.setLocalDateTime(LocalDateTime.now()); 
+		alertMessage.setLocalDateTime(LocalDateTime.now());
 		alertMessage.setSource("Licence Administration");
 		return alertMessage;
 	}
-
 
 	public LicenceKeyContent getLicenceKeyContent() throws DcemException {
 		AdminTenantData adminTenantData = adminModule.getTenantData();
 		LicenceKeyContent licenceKeyContent = adminTenantData.getLicenceKeyContent();
 		if (licenceKeyContent == null) {
-			throw new DcemException (DcemErrorCodes.LICENCE_NOT_AVAILABLE, "");
+			throw new DcemException(DcemErrorCodes.LICENCE_NOT_AVAILABLE, "");
 		}
 		return licenceKeyContent;
 	}
-
 
 	public LicenceKeyContentUsage getTenantLicenceKeyUsage(TenantEntity tenantEntity) throws Exception {
 		Future<LicenceKeyContentUsage> future = taskExecutor.submit(new Callable<LicenceKeyContentUsage>() {
@@ -465,11 +437,9 @@ public class LicenceLogic implements ReloadClassInterface {
 		return future.get();
 	}
 
-
 	public LicenceKeyContentUsage getLicenceKeyContentUsage() throws DcemException {
 		AsModuleApi asModuleApi = (AsModuleApi) CdiUtils.getReference(DcemConstants.AS_MODULE_API_IMPL_BEAN);
-		return new LicenceKeyContentUsage(userLogic.getTotalUserCount(), asModuleApi.getCloudSafeUsageMb(),
-				getLicenceKeyContent());
+		return new LicenceKeyContentUsage(userLogic.getTotalUserCount(), asModuleApi.getCloudSafeUsageMb(), getLicenceKeyContent());
 	}
 
 	@Override
