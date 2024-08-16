f0cbb2e2118fe13ebd779b5a7a0725bd63dc6aba
Galea, Emanuel <emanuel.galea@hws-gruppe.de>
2024-08-16 08:20:54 +0200

    add info to cloudSafeFile

--- DcemCore/src/main/java/com/doubleclue/dcem/core/as/DcemUploadFile.java
+++ DcemCore/src/main/java/com/doubleclue/dcem/core/as/DcemUploadFile.java
@@ -6,10 +6,17 @@ public class DcemUploadFile {
 
 	public String fileName;
 	public File file;
+	public String info;
 
 	public DcemUploadFile(String fileName, File file) {
 		this.fileName = fileName;
 		this.file = file;
+	}
+	
+	public DcemUploadFile(String fileName, File file, String info) {
+		this.fileName = fileName;
+		this.file = file;
+		this.info = info;
 	}
 
 }
