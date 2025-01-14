ba0b98b8d0ca90a7a73995c0af650a71ae9f8185
jonas.kiesewetter <jonas.kiesewetter@hws-gruppe.de>
2025-01-14 10:56:39 +0100

    improved getLengthKb()

--- AppSecureModule/src/main/java/com/doubleclue/dcem/as/entities/CloudSafeEntity.java
+++ AppSecureModule/src/main/java/com/doubleclue/dcem/as/entities/CloudSafeEntity.java
@@ -418,15 +418,21 @@ public class CloudSafeEntity extends EntityInterface implements Cloneable {
 	@Transient
 	@JsonIgnore
 	public String getLengthKb() {
-		if (isFolder) {
-			return "-";
-		} else if (length > 0) {
-			return String.valueOf((length + 1024 - 1) / 1024);
-		} else {
-			return "0";
-		}
-
+	    if (isFolder) {
+	        return "-";
+	    } else if (length > 0) {
+	        long sizeInKb = (length + 1024 - 1) / 1024;
+	        if (sizeInKb >= 1024) {
+	            double sizeInMb = sizeInKb / 1024.0;
+	            return String.format("%.2f MB", sizeInMb); // Größe in MB mit 2 Dezimalstellen
+	        } else {
+	            return sizeInKb + " KB"; // Größe in KB
+	        }
+	    } else {
+	        return "0 KB"; // Standardwert für ungültige Größen
+	    }
 	}
+
 
 	public CloudSafeEntity getParent() {
 		return parent;
